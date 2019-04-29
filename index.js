bcb7217c853465b55f434e546d9af4922063c17a
harsilspatel <harsilspatel@gmail.com>
2019-04-30 00:33:28 +1000

    Update packages and rename bot

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = app => {
   app.on('issues.opened', async context => {
     context.github.issues.get(context.issue()).then(issueResult => {
       const titleList = issueResult.data.title.split(' ').filter(i => i);
-      if (titleList[0] != 'cuntents') {
+      if (titleList[0] != 'contents') {
         return
       }
       const filePayload = context.repo();
@@ -29,7 +29,7 @@ module.exports = app => {
         context.github.markdown.render({text: atob(fileContents.data.content)}).then(htmlBody => {
           const $ = cheerio.load(htmlBody.data);
           const headers = $(":header");
-          let cuntent = '```Markdown\nTABLE OF CONTENTS\n=================\n';
+          let contents = '```Markdown\nTABLE OF CONTENTS\n=================\n';
   
           headers.each(function (i, elem) {
               const tag = $(this).get(0).name;
@@ -37,13 +37,13 @@ module.exports = app => {
               const indent = '  '.repeat(depth);
               const title = $(this).text().trim();
               const href = $(this).find('a').attr("href");
-              cuntent += `\n${indent}* [${title}](${href})`;
+              contents += `\n${indent}* [${title}](${href})`;
           });
   
-          cuntent += '\n```'
-          // console.log(cuntent)
+          contents += '\n```'
+          // console.log(contents)
           
-          const issue = context.issue({body: cuntent})
+          const issue = context.issue({body: contents})
           return context.github.issues.createComment(issue)
         }) //context.github.markdown.render  
       }) //context.github.repos.getContents
